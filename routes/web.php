f344c8f29558c9858e5257b2076c990d3803db63
wgs <wgs_eu@hotmail.com>
2019-09-16 16:26:14 -0300

    Integrado Laravel/GraphQl

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
-use Symfony\Component\Routing\RouteCollection;
 
-$main_collection = new RouteCollection();
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-\MZ\System\PaginaPageController::addRoutes($main_collection);
-
-$main_collection->addCollection(require_once(__DIR__ . '/api.php'));
-$main_collection->addCollection(require_once(__DIR__ . '/old_api.php'));
-
-return $main_collection;
+Route::get('/', function () {
+    return view('index');
+});

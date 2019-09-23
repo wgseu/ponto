bc5b26f23a324a26eea5b8447d7a5c7009a8e75e
wgs <wgs_eu@hotmail.com>
2019-09-23 14:20:09 -0300

    Tymonjwt e adicionados providers.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'cors' => \App\Http\Middleware\Cors::class,
+        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 
     /**

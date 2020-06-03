0d13210d13e082f5682b7aaf283ccd312dfcd524
wgs <wgs_eu@hotmail.com>
2020-06-02 21:35:58 -0300

    Adicionado novos parametros na criação do ponto

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -12,6 +12,10 @@ class PontoController extends Controller
         return Ponto::create([
             'colaborador_id' => $request->colaborador_id,
             'data_ponto' => \Carbon\Carbon::now(),
+            'latitude' => $request->latitude ?? null,
+            'longitude' => $request->longitude ?? null,
+            'anexo_url' => $request->aneno_url ?? null,
+            'descricao' => $request->descricao ?? null,
         ]);
     }
 }
